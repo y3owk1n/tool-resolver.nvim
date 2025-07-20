24a88060b34eeae69ee5223df9b6ebb448cd5c27
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-07-20 11:36:05 +0800

    fix(docs): switch from `pandocvim` to `vimcats` for docs generation (#28)
    * fix(docs): switch from `pandocvim` to `vimcats` for docs generation
    
    * ensure name consistent
    
    * add stable toolchain
    
    * try just using vim to generate
    
    * chore(build): auto-generate docs from readme
    
    This is an automated commit.

--- Justfile
+++ Justfile
@@ -1,5 +1,11 @@
 doc:
-    panvimdoc --project-name tool-resolver.nvim --input-file ./README.md --demojify true --vim-version "Neovim >= 0.11.0"
+    vimcats -t -f -c -a \
+    lua/tool-resolver/init.lua \
+    lua/tool-resolver/config.lua \
+    lua/tool-resolver/types.lua \
+    > doc/tool-resolver.nvim.txt
+
+    vim -c "helptags doc" -c "q"
 
 set shell := ["bash", "-cu"]
 
--- lua/tool-resolver/types.lua
+++ lua/tool-resolver/types.lua
@@ -1,15 +1,28 @@
+---@mod tool-resolver.types Types
+---
+
+local M = {}
+
+---Config
 ---@class ToolResolver.Config
 ---@field tools table<string, ToolResolver.Config.Tools> tools with type and fallback
 
+---Config Tools
 ---@class ToolResolver.Config.Tools
 ---@field type ToolResolver.ResolverType
 ---@field fallback? string fallback binary name, if not specified then use the key.
 
+---`get_bin` options
 ---@class ToolResolver.GetBinOpts
 ---@field path? string start search path (default: current buffer)
 
----@alias ToolResolver.ResolverType "node" -- add more in future
+---Resolver types
+---@alias ToolResolver.ResolverType
+---| '"node"' # node-based resolver (e.g. using node_modules)
 
+---Resolver metadata
 ---@class ToolResolver.ResolverMeta
 ---@field root_markers string[] root_markers
 ---@field bin_subpath string[] bin_subpath
+
+return M
